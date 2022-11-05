e2abe7ae4421ae419a2fa587961fdbfa80c65a25
Deena <pdeena180@gmail.com>
2022-11-05 15:21:25 +0530

    login page

--- js/login.js
+++ js/login.js
@@ -1,15 +1,23 @@
-async function signin(){
-    let email = document.getElementById("inputEmail")
-    let password = document.getElementById("inputPassword")
+function signin(){
+    let email = document.getElementById("inputEmail").value
+    let password = document.getElementById("inputPassword").value
+    console.log(email)
     let logincred = {
         email:email,
         password:password
     }
     let login = JSON.stringify(logincred)
-    let xhttp = XMLHttpRequest()
-    xhttp.open("GET", "localhost:8080/login.php",true)
-    xhttp.send(login)
-    let data = xhttp.responseText;
-    if(data==0)
-        window.open("profile.html")
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("POST","php/login.php",true)
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhttp.onreadystatechange = function(){
+        if( xhttp.readyState == 4 && xhttp.status == 200){
+            console.log(xhttp.response)
+        }
+    }
+    xhttp.send("cred="+login)
+    // let data = xhttp.responseText;
+    // console.log(data)
+    // if(data==0)
+    //     window.open("profile.html")
 }
--- php/login.php
+++ php/login.php
@@ -1,11 +1,15 @@
 <?php
-    $email = $_GET['email']
-    $password = $_GET['password']
-    $conn = new mysqli("localhost","root","pass","guvi");
-    $result = $conn->query("SELECT * FROM login")
-    if ($result.password == $password) {
-        $data = 0;
-    } 
-    $conn->close();
-    echo json_encode($data)
+    if(!isset($_POST['cred'])){
+        include (__DIR__."\..\login.html");
+    }else{
+        $input = $_POST['cred'];
+        $cred = json_decode($input);
+        $conn = new mysqli("localhost","root","","guvi");
+        $stmt = $conn->prepare("SELECT * FROM Login WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $email = $cred->email;
+        $stmt->execute();
+        $result = $stmt->get_result();
+        echo json_encode($result->fetch_assoc());
+    }
 ?>
